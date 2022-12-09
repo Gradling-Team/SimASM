58cc41d1307eaa25d6efcadbc62a63a7d062fdaa
MathieuChntt <mathieuelouan@gmail.com>
2022-12-09 01:01:33 +0100

    avec le main qui lit le fichier

--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
+using namespace std;
+#include "fileReader.cpp"
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
+    FileReader fileReader;
+    fileReader.read("example.txt");
+
     return 0;
 }
