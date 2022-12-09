e29c2cca80eee689fea04591717a51e725a80d6f
MathieuChntt <mathieuchntt@users.noreply.github.com>
2022-12-09 23:03:17 +0100

    Add SyntaxChecker branch

---
+++ src/SyntaxChecker.cpp
@@ -0,0 +1,5 @@
+// Source File for class SyntaxChecker
+// Created by MathieuCtt on 09/12/2022.
+// Copyright (c) 2022 Gradling Team. All rights reserved.
+
+#include "SyntaxChecker.hpp"
---
+++ src/SyntaxChecker.hpp
@@ -0,0 +1,15 @@
+// Header file for SyntaxChecker.cpp
+// Created by MathieuCtt on 09/12/2022.
+// Copyright (c) 2022 Gradling Team. All rights reserved.
+
+
+#ifndef SIMASM_SYNTAXCHECKER_HPP
+#define SIMASM_SYNTAXCHECKER_HPP
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+
+
+#endif //SIMASM_SYNTAXCHECKER_HPP
